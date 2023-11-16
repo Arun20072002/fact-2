a6afe0ad6e62b99c8868de5eff3a2715ba8e22c5
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-11-16 15:12:04 +0530

    fact-2

---
+++ fact-2.js
@@ -0,0 +1,8 @@
+document.write(" Find the Factorial upto 5 (1! to 5!)"+"<br>")
+document.write("-------------------------------------"+"<br>")
+
+var a=1;
+for(let i=1;i<=5;i++){
+    a=i*a;
+    document.write(a+"<br>")
+}
